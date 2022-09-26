9daec860f7151682afd43ba321dad42e91ceeeeb
newdas <917152124@qq.com>
2022-09-26 16:44:17 +0800

    添加 vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+// 1. Define route components.
+// These can be imported from other files
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  {path: '/test', component: import('@/views/test/Test.vue')},
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+export default router
